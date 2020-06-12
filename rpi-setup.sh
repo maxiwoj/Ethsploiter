80d7e12651d33ac6ebdcf6db8821a87f9355a01f
Maksymilian Wojczuk <maxwojczuk@gmail.com>
2020-06-12 14:37:58 +0200

    Cleaned up logging and fixed sshbrute force

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -1,11 +1,14 @@
 # Load module dwc2
 echo "dtoverlay=dwc2" >> /boot/config.txt
 
+mkdir runtime
+
 cd /home/pi
 dd if=/dev/zero of=usbdisk.img bs=1024 count=1024
 mkdosfs ~/usbdisk.img
 
 
-apt-get install expect hydra
+
+apt-get install expect hydra sshpass
 
 pip install scapy impacket 
